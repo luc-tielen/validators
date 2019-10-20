2b3bc9a405f45dfd6e07c4358ebb41a9e4b2e4b7
Luc Tielen <luc.tielen@kabisa.nl>
2019-10-20 17:11:40 +0200

    Add docs for Data.Validation

--- lib/Data/Validation.hs
+++ lib/Data/Validation.hs
@@ -1,20 +1,53 @@
-module Data.Validation (Validation (..)) where
+-- | This module defines the 'Validation' data type.
+module Data.Validation
+  ( Validation (..),
+    toEither,
+    fromEither,
+  )
+where
 
--- | Used for combining multiple validators together
-data Validation e a
-  = Failure e
+-- | A 'Validation' is very similar to 'Either' in that it contains a value of type /e/ or /a/.
+-- In contrast to Either however, Validation accumulates all errors it comes
+-- across in it's 'Applicative' instance.
+--
+-- A complete example where Validation is used can be found
+-- <https://github.com/luc-tielen/validators.git here>.
+data Validation err a
+  = Failure err
   | Success a
   deriving (Eq, Show)
 
-instance Functor (Validation e) where
-  fmap _ (Failure e) = Failure e
-  fmap f (Success a) = Success $ f a
+instance Functor (Validation err) where
+  fmap _ (Failure err) = Failure err
+  fmap f (Success a) = Success (f a)
 
-instance Semigroup e => Applicative (Validation e) where
+instance Semigroup err => Applicative (Validation err) where
 
   pure = Success
 
   Failure e1 <*> Failure e2 = Failure $ e1 <> e2
   Failure e1 <*> _ = Failure e1
   _ <*> Failure e2 = Failure e2
-  Success f <*> Success a = Success $ f a
+  Success f <*> Success a = Success (f a)
+
+-- | Conversion function from 'Validation' to 'Either'.
+--
+-- >>> toEither (Failure 1)
+-- Left 1
+-- >>> toEither (Success True)
+-- Right True
+toEither :: Validation err a -> Either err a
+toEither v = case v of
+  Failure e -> Left e
+  Success a -> Right a
+
+-- | Conversion function from 'Either' to 'Validation'.
+--
+-- >>> fromEither (Left 1)
+-- Failure 1
+-- >>> fromEither (Right True)
+-- Success True
+fromEither :: Either err a -> Validation err a
+fromEither e = case e of
+  Left a -> Failure a
+  Right b -> Success b
--- lib/Data/Validator.hs
+++ lib/Data/Validator.hs
@@ -1,18 +1,23 @@
 -- | This module defines the 'Validator' data type and helper functions
 --   for creating various validators.
-module Data.Validator (Validator, validate, assert, refute) where
+module Data.Validator
+  ( Validator,
+    validate,
+    assert,
+    refute,
+  )
+where
 
 import Data.Validation
 
-{-| Helper data type for keeping track of errors.
-
-Only used internally in the 'Validator' type to keep track of accumulated errors.
--}
-data Result err
-  = Ok
-  -- ^ Validator returned no error.
-  | Err err
-  -- ^ Validator returned an error.
+-- | Helper data type resembling the result of a 'Validator' assertion.
+--
+-- There are only 2 possible results:
+-- 1. 'Ok': the validator assertion succeeded.
+-- 2. 'Err': the validator assertion failed.
+--
+-- Only used internally in the 'Validator' type to keep track of accumulated errors.
+data Result err = Ok | Err err
   deriving (Eq, Show)
 
 instance Semigroup err => Semigroup (Result err) where
@@ -24,18 +29,17 @@ instance Semigroup err => Semigroup (Result err) where
 instance Monoid err => Monoid (Result err) where
   mempty = Ok
 
-{-| Datatype for checking if a validation holds for a subject.
-
-- __subject__ can be any data type for which assertions need to be checked.
-- __err__ can be any type representing an error, but it will only be possible to
-combine validators if the error type has a Semigroup instance.
-
-Execute a validator by passing it to the 'validate' function.
-
-A Validator is both a 'Semigroup' and a 'Monoid', making it possible to combine
-smaller validators into larger validators. A combined validator will accumulate
-errors from all of it's sub-validators.
--}
+-- | Datatype for checking if a validation holds for a subject.
+--
+-- - __subject__ can be any data type for which assertions need to be checked.
+-- - __err__ can be any type representing an error, but it will only be possible to
+-- combine validators if the error type has a Semigroup instance.
+--
+-- Execute a validator by passing it to the 'validate' function.
+--
+-- A Validator is both a 'Semigroup' and a 'Monoid', making it possible to combine
+-- smaller validators into larger validators. A combined validator will accumulate
+-- errors from all of it's sub-validators.
 newtype Validator err subject = Validator (subject -> Result err)
 
 instance Semigroup err => Semigroup (Validator err subject) where
@@ -44,40 +48,37 @@ instance Semigroup err => Semigroup (Validator err subject) where
 instance Monoid err => Monoid (Validator err subject) where
   mempty = Validator mempty
 
-{-| Runs a validator on a subject.
-
-The result is a 'Validation' containing all accumulated errors,
-or the subject wrapped in a 'Success' value.
--}
+-- | Runs a validator on a subject.
+--
+-- The result is a 'Validation' containing all accumulated errors,
+-- or the subject wrapped in a 'Success' value.
 validate :: Validator err subject -> subject -> Validation err subject
 validate (Validator f) a = case f a of
   Err e -> Failure e
   Ok -> Success a
 
-{-| Creates a validator that will return an error if the given predicate doesn't hold.
-
-Usage:
-
->>> let validator = assert (> 10) ["too small"]
->>> validate validator 11
-Success 11
-
->>> validate validator 1
-Failure ["too small"]
--}
+-- | Creates a validator that will return an error if the given predicate doesn't hold.
+--
+-- Usage:
+--
+-- >>> let validator = assert (> 10) ["too small"]
+-- >>> validate validator 11
+-- Success 11
+--
+-- >>> validate validator 1
+-- Failure ["too small"]
 assert :: (subject -> Bool) -> err -> Validator err subject
 assert p err = Validator $ \subject -> if p subject then Ok else Err err
 
-{-| Creates a validator that will return an error if the given predicate holds.
-
-Usage:
-
->>> let validator = refute (> 10) ["too big"]
->>> validate validator 11
-Failure ["too big"]
-
->>> validate validator 1
-Success 1
--}
+-- | Creates a validator that will return an error if the given predicate holds.
+--
+-- Usage:
+--
+-- >>> let validator = refute (> 10) ["too big"]
+-- >>> validate validator 11
+-- Failure ["too big"]
+--
+-- >>> validate validator 1
+-- Success 1
 refute :: (subject -> Bool) -> err -> Validator err subject
 refute p = assert (not . p)
